84791a3e5da097b7f2eadf2cee2d83d2397aae7f
Lavish <lavishjain36@gmail.com>
2022-08-05 14:49:26 +0530

    Otp Validation

--- script.js
+++ script.js
@@ -1,35 +1,18 @@
-async function fun(){
-    try{
-        //Get the input from the user
-       
-        let code=document.getElementById("num").value; 
-        if(code.length>6||code.length<6){
-            alert("Please enter a 6 digit code");
-            
-        }
-        //clear the output field before displaying the result
-        // document.getElementById("district").innerHTML="";
-        // document.getElementById("state").innerHTML="";
-        // document.getElementById("country").innerHTML="";
+let digitValidate = function(ele){
+    console.log(ele.value);
+    ele.value = ele.value.replace(/[^0-9]/g,'');//Regular expression to allow only numbers
+  }
+  
+  let tabChange = function(val){
+      let ele = document.querySelectorAll('input');
+      if(ele[val-1].value != ''){
+        ele[val].focus()
+      }else if(ele[val-1].value == ''){
+        ele[val-2].focus()
+      }   
+   }
+  
 
-        //Get the data from the API
-        let response=await fetch(`https://api.postalpincode.in/pincode/${code}`);
-        let data=await response.json();
-        //Display the data in the output field
-        // console.log(data[0].PostOffice[0].State);
-        // console.log(data[0].PostOffice[0].District);
-        // console.log(data[0].PostOffice[0].Name);
-        if(data[0].PostOffice[0]==null){
-            alert("Please enter a valid code");
-        }else{
-            document.getElementById("state").value=data[0].PostOffice[0].State;
-            document.getElementById("district").value=data[0].PostOffice[0].District;
-            document.getElementById("area").value=data[0].PostOffice[0].Name;
-        }
-
-        document.getElementById("num").value="";
-    }catch(e){
-        alert("Please enter a valid code");
-       
-    }
-}
+//    if the data is empty then it will focus on the previous input field.  
+//    if the data is not empty then it will focus on the next input field.
+  
---
+++ test.py
@@ -0,0 +1,10 @@
+# Print the First 3 multiples of the given number "N"
+n=int(input("Enter the number: "))
+for i in range(1,4):
+    print(n*i)
+print("\n")
+
+n=int(input(""))
+for i in range(1,4):
+    print(n*i,end=" ")
+
